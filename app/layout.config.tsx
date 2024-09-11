cfb5ddc0b0f81c7616ad29fb9c46562d4eef1d13
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-09-11 14:52:01 +0800

    在应用布局中添加灵动算力logo
       在`layout.config.tsx`中实现了一个新的`logo`函数，该函数使用Next.js的`Image`组件和自定义样式展示公司logo。此外，将`nav.title`属性从文本更改为组件，以使用新增的logo组件。
    
    2. Next.js配置中允许图像优化
    
       修改`next.config.mjs`以启用图像优化。通过设置`images: { unoptimized: true }`，解决了之前配置中未优化图像显示的问题。

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,25 +1,34 @@
-import { type DocsLayoutProps } from 'fumadocs-ui/layout';
-import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
-import { pageTree } from '@/app/source';
+import { type DocsLayoutProps } from 'fumadocs-ui/layout'
+import { type HomeLayoutProps } from 'fumadocs-ui/home-layout'
+import { pageTree } from '@/app/source'
+import Image from 'next/image'
+import logoimage from './20240911-143807.png'
+
+function logo() {
+	return (
+		<div className='flex flex-row items-center'>
+			<Image src={logoimage} width={48} height={48} alt="灵动算力" />
+      <div>灵动算力</div>
+		</div>
+	)
+}
 
 // shared configuration
 export const baseOptions: HomeLayoutProps = {
-  nav: {
-    title: '灵动算力租赁',
-  },
-  // links: [
-  //   {
-  //     text: 'Documentation',
-  //     url: '/docs',
-  //     active: 'nested-url',
-  //   },
-  // ],
-};
-
-
+	nav: {
+		title: logo(),
+	},
+	// links: [
+	//   {
+	//     text: 'Documentation',
+	//     url: '/docs',
+	//     active: 'nested-url',
+	//   },
+	// ],
+}
 
 // docs layout configuration
 export const docsOptions: DocsLayoutProps = {
-  ...baseOptions,
-  tree: pageTree,
-};
+	...baseOptions,
+	tree: pageTree,
+}
