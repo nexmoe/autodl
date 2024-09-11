f6149681e8e33177111e8774937a48fdafd5fcea
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-09-11 14:55:35 +0800

    调整CTA按钮组件布局和取消按钮位置
    在CTAButton组件中，将取消按钮移至AlertDialogTitle内，并使其居于标题的末尾。同时，增加了iframe的高度并调整其类，以改善等待列表注册表单的展示效果。这些布局调整旨在提升用户界面的直观性和响应性。

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,10 +1,8 @@
 import {
 	AlertDialog,
-	AlertDialogAction,
 	AlertDialogCancel,
 	AlertDialogContent,
 	AlertDialogDescription,
-	AlertDialogFooter,
 	AlertDialogHeader,
 	AlertDialogTitle,
 	AlertDialogTrigger,
@@ -23,14 +21,14 @@ export default function () {
 			</AlertDialogTrigger>
 			<AlertDialogContent className="max-w-3xl">
 				<AlertDialogHeader>
-					<AlertDialogTitle>加入等待列表</AlertDialogTitle>
+					<AlertDialogTitle className='flex flex-row justify-between items-center'>加入等待列表<AlertDialogCancel>关闭</AlertDialogCancel></AlertDialogTitle>
 					<AlertDialogDescription>
 						请在下方表单中填写您的信息,我们会在服务推出时第一时间通知您。
 					</AlertDialogDescription>
 				</AlertDialogHeader>
 				<div className="flex-1 overflow-hidden h-full">
 					<iframe
-						className="h-[60vh]"
+						className="h-[70vh] rounded-md"
 						src="https://fizuclq6u3i.feishu.cn/share/base/form/shrcnbxdW0U5iCqTxCXnokODfRe"
 						width="100%"
 						height="100%"
@@ -41,9 +39,6 @@ export default function () {
 						加载中...
 					</iframe>
 				</div>
-				<AlertDialogFooter>
-					<AlertDialogCancel>关闭</AlertDialogCancel>
-				</AlertDialogFooter>
 			</AlertDialogContent>
 		</AlertDialog>
 	)
